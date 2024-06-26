3390129ce4ee40e2a7287a69e37b3a64646ebca9
Shamveel P <shamveelp007@gmail.com>
2024-06-26 15:43:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+const decreasebtn = document.getElementById("decreasebtn");
+const increasebtn = document.getElementById("increasebtn");
+const resetbtn = document.getElementById("resetbtn");
+const clabel = document.getElementById("clabel");
+
+let count = 0;
+
+increasebtn.onclick = function(){
+    count++;
+    clabel.textContent = count;
+}
+
+resetbtn.onclick = function() {
+    count = 0;
+    clabel.textContent = count
+}
+
+decreasebtn.onclick = function() {
+    if (count > 0) {
+    count--;
+    clabel.textContent = count;
+    }
+}
