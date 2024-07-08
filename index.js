67c7226cfbd441399835c099cf0aaa509915251a
Shamveel P <shamveelp007@gmail.com>
2024-07-08 13:22:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const clabel = document.getElementById("clabel");
 
 let count = 0;
 
-increasebtn.onclick = function(){
-    count++;
-    clabel.textContent = count;
-}
-
-resetbtn.onclick = function() {
-    count = 0;
-    clabel.textContent = count
-}
-
 decreasebtn.onclick = function() {
     if (count > 0) {
     count--;
     clabel.textContent = count;
     }
 }
+
+resetbtn.onclick = function() {
+    count = 0;
+    clabel.textContent = count
+}
+increasebtn.onclick = function(){
+    count++;
+    clabel.textContent = count;
+}
+
